280740dd5c131febd4eee0544e372e1a96e63124
Harishm2010 <145553810+Harishm2010@users.noreply.github.com>
2023-12-20 11:43:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,40 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
 
-streamlit.title('My new healthy diner spot')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast with blue cheese')
+"""
+# Welcome to Streamlit!
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-# Display the table on the page
-streamlit.dataframe(fruits_to_show)
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
 
-def get_fruityvice_data(this_fruit_choice):
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
 
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-	return (fruityvice_normalized)
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
+})
 
-
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-	fruit_choice = streamlit.text_input('What fruit would you like information about?')
-	if not fruit_choice:
-		streamlit.error("please select a fruit to get information.")
-	else:
-		back_from_function = get_fruityvice_data(fruit_choice)
-		streamlit.dataframe(back_from_function)
-except URLError as e:
-    streamlit.error()
-
-streamlit.stop()
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
